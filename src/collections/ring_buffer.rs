844b01ab9faed6da36d4771352ed84cc3d86f2da
Gregory Langlais <general@gregorylanglais.com>
2020-10-09 12:41:41 -0500

    update buffer size and slice for debug

--- src/collections/ring_buffer.rs
+++ src/collections/ring_buffer.rs
@@ -2,25 +2,27 @@ use std::fmt;
 
 use crate::types::event::Event;
 
-const DEFAULT_BUFFER_SIZE: usize = 33;
+const BUFFER_SIZE: usize = 1_000_000;
+const DEBUG_SLICE_SIZE: usize = 10;
 
 #[derive(Copy, Clone)]
 pub struct RingBuffer {
-    buffer: [Event; DEFAULT_BUFFER_SIZE],
+    buffer: [Event; BUFFER_SIZE],
 }
 
 impl RingBuffer {
     pub fn new() -> RingBuffer {
         RingBuffer {
-            buffer: [Event::new(); DEFAULT_BUFFER_SIZE],
+            buffer: [Event::new(); BUFFER_SIZE],
         }
     }
 }
 
 impl fmt::Debug for RingBuffer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let buffer_slice = &self.buffer[0..DEBUG_SLICE_SIZE];
         let mut out = String::new();
-        for (i, event) in self.buffer.iter().enumerate() {
+        for (i, event) in buffer_slice.iter().enumerate() {
             out += &format!("\n\t{}: {:?}", i, event);
         }
         write!(f, "RingBuffer [{}\n]", out)
