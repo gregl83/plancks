7db4428f36b6aa42ae9a1e8cd087a0fd84d18b41
Gregory Langlais <general@gregorylanglais.com>
2020-10-12 21:15:33 -0500

    add buffer index wip

--- src/collections/ring_buffer.rs
+++ src/collections/ring_buffer.rs
@@ -3,26 +3,90 @@ use std::fmt;
 use crate::types::event::Event;
 
 const BUFFER_SIZE: usize = 1_000_000;
-const BUFFER_INDEX_DEFAULT: usize = 0;
 const DEBUG_SLICE_SIZE: usize = 10;
+
+// operate in series or parallel?
+// maintain ordering of consumption
+// api for accessing the next element of stage (index)
+
+// event 1: stage: a
+// event 2: stage c
+// event 3: stage b
+
+// states are array with index? maybe states doesn't need to be dynamic?
+
+// index per stage (locking at some point for ranges)
+
+// stage implies ordering
+// state implies how it exists
+// ring_buffer.next_in_state(state)
+// ring_buffer.idx = state{  }
+
+
+// States in Ring Buffer
+// 1. journal
+// 2. replicate
+// 3. unmarshall
+
+
+// state index?
+// state a
+// state b
+// state c
+//
+
+#[derive(Copy, Clone)]
+pub struct BufferIndex {
+    pub key: usize,
+}
+
+impl BufferIndex {
+    pub fn new() -> BufferIndex {
+        BufferIndex {
+            key: 0
+        }
+    }
+}
+
+// todo
+// write index
+// state index
+// read index
 
 #[derive(Copy, Clone)]
 pub struct RingBuffer {
-    buf: [Event; BUFFER_SIZE],
-    idx: usize,
+    // buffer of events
+    buf: [
+        Event;
+        BUFFER_SIZE
+    ],
+
+    // index for reads
+    idx_r: BufferIndex,
+
+    // index for writes
+    idx_w: BufferIndex,
 }
 
 impl RingBuffer {
     pub fn new() -> RingBuffer {
         RingBuffer {
-            buf: [Event::new(); BUFFER_SIZE],
-            idx: BUFFER_INDEX_DEFAULT,
+            buf: [
+                Event::new();
+                BUFFER_SIZE
+            ],
+            idx_r: BufferIndex::new(),
+            idx_w: BufferIndex::new(),
         }
     }
 
     pub fn len(&self) -> usize { self.buf.len() }
 
-    pub fn index(&self) -> usize { self.idx }
+    pub fn write(&self, _event: Event) {
+        // fixme - implement lifetime parameter
+        // self.buf[self.idx_w.key].write(); // fixme - chose write type
+        // self.idx_w.key += 1;
+    }
 }
 
 impl Iterator for RingBuffer {
@@ -30,11 +94,11 @@ impl Iterator for RingBuffer {
 
     fn next(&mut self) -> Option<Self::Item> {
         // todo - return ready values
-        if self.idx == self.buf.len() {
-            self.idx = BUFFER_INDEX_DEFAULT;
+        if self.idx_r.key == self.buf.len() {
+            self.idx_r.key = 0;
         }
-        let res = Some(self.buf[self.idx]);
-        self.idx += 1;
+        let res = Some(self.buf[self.idx_r.key]);
+        self.idx_r.key += 1;
         res
     }
 }
--- src/types/event.rs
+++ src/types/event.rs
@@ -1,15 +1,38 @@
 use std::fmt;
 
 #[derive(Copy, Clone)]
+pub struct EventState {
+    journaled: bool,
+    replicated: bool,
+    unmarshalled: bool,
+}
+
+impl EventState {
+    pub fn new() -> EventState {
+        EventState {
+            journaled: false,
+            replicated: false,
+            unmarshalled: false,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
 pub struct Event {
-    locked: bool
+    locked: bool,
+    state: EventState
 }
 
 impl Event {
     pub fn new() -> Event {
         Event {
             locked: false,
+            state: EventState::new()
         }
+    }
+
+    pub fn write(&mut self) {
+        self.state = EventState::new();
     }
 }
 
